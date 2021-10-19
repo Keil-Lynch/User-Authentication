d1b4ce8dc4a580cb6ea2d1e3ee84b49aa0815c22
Keil-Lynch <85366371+Keil-Lynch@users.noreply.github.com>
2021-10-19 15:10:26 -0400

    Add files via upload

---
+++ User Authentication/User Authentication.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	char password[10], username[10], ch;
+	int i;
+
+	printf("Enter User name: ");
+	gets(username);
+	printf("Enter the password < any 8 characters>: ");
+	for (i = 0; i < 9; i++)
+	{
+            ch = getchar();
+            password[i] = ch;
+            ch = '*' ;
+            printf("%c", ch);
+	}
+        password[i] = '\0';
+
+	printf("\n Your password is :");
+	for (i = 0; i < 9; i++)
+	{
+            printf("%c", password[i]);
+	}
+}
